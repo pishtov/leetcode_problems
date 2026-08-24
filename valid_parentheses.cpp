540b8019a4fd24ef59404314b7c23319cf196c45
Dimitar Iliev <121585758+pishtov@users.noreply.github.com>
2026-08-24 20:24:51 +0300

    Improve parentheses validation logic
    Refactor isValid function to use unordered_map for pairs.

--- valid_parentheses.cpp
+++ valid_parentheses.cpp
@@ -35,22 +35,24 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        
-        for (int i = 0; i < s.length(); i++) {
-        char c = s[i];
-            if (c == '(' || c == '{' || c == '[') {
+
+        unordered_map<char, char> pairs = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+     
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
                 st.push(c);
-            } else {
-                if (st.empty()) {
-                    return false;
-                }
-            if ( c == ')' && st.top() == '(' || c == ']' && st.top() == '[' || c == '}' && st.top() == '{' ) {
-                st.pop();
-            } else {
+                continue;
+            }
+         
+            if (st.empty() || st.top() != pairs[c]) {
                 return false;
             }
+            st.pop();
         }
+        return st.empty();
     }
-    return st.empty();
-}
 };
