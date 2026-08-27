38548b70ce2f2551a15ef60001471fd622a4fca0
Dimitar Iliev <121585758+pishtov@users.noreply.github.com>
2026-08-27 21:08:21 +0300

    Change single-line comments to block comments
    Updated comments to use block comment style.

--- divisor_game.cpp
+++ divisor_game.cpp
@@ -1,4 +1,4 @@
-Alice and Bob take turns playing a game, with Alice starting first.
+/* Alice and Bob take turns playing a game, with Alice starting first.
 
 Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
 
@@ -7,8 +7,6 @@ Replacing the number n on the chalkboard with n - x.
 Also, if a player cannot make a move, they lose the game.
 
 Return true if and only if Alice wins the game, assuming both players play optimally.
-
- 
 
 Example 1:
 
@@ -21,7 +19,6 @@ Input: n = 3
 Output: false
 Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
  
-
 Constraints:
 
-1 <= n <= 1000
+1 <= n <= 1000 */
