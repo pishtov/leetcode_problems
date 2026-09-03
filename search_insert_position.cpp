b341b071af569ab153305a215554e392747df7b2
Dimitar Iliev <121585758+pishtov@users.noreply.github.com>
2026-09-03 15:31:21 +0300

    Implement binary search for insert position

--- search_insert_position.cpp
+++ search_insert_position.cpp
@@ -14,3 +14,24 @@ Constraints:
 -104 <= nums[i] <= 104
 nums contains distinct values sorted in ascending order.
 -104 <= target <= 104 */
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+};
