02005b36a641f6cabe0d522889b21d696611b0f9
Dimitar Iliev <121585758+pishtov@users.noreply.github.com>
2026-09-04 14:58:57 +0300

    Implement binary addition in addBinary function

--- add_binary.cpp
+++ add_binary.cpp
@@ -14,3 +14,33 @@ Constraints:
 1 <= a.length, b.length <= 104
 a and b consist only of '0' or '1' characters.
 Each string does not contain leading zeros except for the zero itself. */
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+
+            if (i >= 0)
+                sum += a[i] - '0';
+
+            if (j >= 0)
+                sum += b[j] - '0';
+
+            result += (sum % 2) + '0';
+            carry = sum / 2;
+
+            i--;
+            j--;
+        }
+
+        reverse(result.begin(), result.end());
+
+        return result;
+    }
+};
